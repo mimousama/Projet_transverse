ea703bbfbdb86f507683d3afda59255fb14b6532
RaphaelFS1 <rapha.freser@gmail.com>
2026-03-20 12:21:37 +0100

    Started the identification process, made some improvments

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,3 +21,40 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 console.log("All connected yiiihaaaa");
+
+// 3. Récupération des éléments HTML
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const btnSignup = document.getElementById('btn-signup');
+const btnLogin = document.getElementById('btn-login');
+const messageBox = document.getElementById('message');
+
+// --- FONCTION INSCRIPTION ---
+btnSignup.addEventListener('click', () => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            messageBox.style.color = "green";
+            messageBox.innerText = "Compte créé pour : " + userCredential.user.email;
+        })
+        .catch((error) => {
+            messageBox.innerText = "Erreur : " + error.message;
+        });
+});
+
+// --- FONCTION CONNEXION ---
+btnLogin.addEventListener('click', () => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            messageBox.style.color = "blue";
+            messageBox.innerText = "Bienvenue, tu es connecté !";
+        })
+        .catch((error) => {
+            messageBox.innerText = "Erreur : " + error.message;
+        });
+});
